2e50a348eb3ee30c228b1e0103753c2a8315ebda
Sean Belverstone <seanbelverstone@gmail.com>
2019-11-09 16:36:36 -0600

    Was going to put in a switch case to add an image depending on what the result says, but just deleted this instead. Will implement later

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 
-const MainCard = (props) => {
+const MainCard = (props) => { 
+
+
 	return (
 		<div>
 			<Card>
-				<CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
 				<CardBody>
 					<CardTitle>{`Title: ${props.title} ID: ${props.reference_id}`}</CardTitle>
 					<CardSubtitle>{`Notes ${props.notes}`}</CardSubtitle>
