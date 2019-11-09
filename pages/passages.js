faa479b4dbedbc3797206caf7b9d3454dfd4aa2d
Sean Belverstone <seanbelverstone@gmail.com>
2019-11-09 15:36:36 -0600

    Removed the semi colon rule from eslint as it was bugging me. Now all my API calls are made and they return data. This data, for the passages page, is being stored in state for now so I can map through it later. Need to create the card component

--- pages/passages.js
+++ pages/passages.js
@@ -1,23 +1,43 @@
 import React from "react";
 import Head from "next/head";
 import MainNav from "../components/Navbar";
+import API from "../utils/API";
 
-const Passages = () => (
-	<div>
-		<Head>
-			<title>Passages</title>
-			<link rel='icon' href='/favicon.ico' />
-		</Head>
+class Passages extends React.Component {
 
-		<MainNav />
-		<div className='hero'>
-			<h1 className='title'>Passages</h1>
+	state = {
+		passageResults: []
+	}
 
-			<div className='row'>
-        
+
+	componentWillMount = () => {
+		API.getPassages().then(results => {
+			console.log(results.data.data)
+			this.setState({passageResults: results.data.data});
+		});
+	
+	}
+
+	render() {
+		return(
+			<div>
+				<Head>
+					<title>Passages</title>
+					<link rel='icon' href='/favicon.ico' />
+				</Head>
+
+				<MainNav />
+				<div className='hero'>
+					<h1 className='title'>Passages</h1>
+
+					<div className='row'>
+				
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
-);
+		)
+	}
+	
+};
 
 export default Passages;
---
+++ utils/API.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export default {
+
+	getPassages: () => {
+		return axios.get("http://18.237.242.89/api/passages")
+	},
+
+	getProblems: () => {
+		return axios.get("http://18.237.242.89/api/problems").then(results => {
+			console.log(results.data.data)
+		})
+	}
+
+};
