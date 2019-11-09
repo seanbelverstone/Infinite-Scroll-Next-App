78c59d507f5d3b1e300b7ed497b0215667d9f24e
Sean Belverstone <seanbelverstone@gmail.com>
2019-11-09 13:36:57 -0600

    Added eslint to ensure code is neat. Also installed reactstrap and imported a navbar component. Thinking about using state to set the title of the page, but may just use props

---
+++ components/Navbar/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import Link from "next/link";
+
+const MainNav = (props) => {
+
+    state = {
+        title: ""
+    }
+
+
+
+    
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <div>
+      <Navbar color="faded" light>
+        <NavbarBrand href="/" className="mr-auto">Navbar</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <Link href="/index/">Home</Link>
+            </NavItem>
+            <NavItem>
+                <Link href="/passages/">Passages</Link>
+            </NavItem>
+            <NavItem>
+                <Link href="/problems/">Problems</Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default MainNav;
